036a1793828ba28cee4d583d1cc4d6567bbe986d
Jie <jie.gillet@gmail.com>
2018-06-11 10:25:40 +0900

    sbatch template replaced by string interpolation

--- sbatch.py
+++ sbatch.py
@@ -2,35 +2,34 @@
 This file is a library for creating a sbatch job script
 """
 
-def import_template(filepath):
-    """
-    This function returns the template located at filepath as a string.
-    """
-    with open(filepath, "r") as f:
-        return f.read()
+template = """
+#!/bin/bash
 
-def export_script(filepath, script):
-    """
-    This function writes the sbatch job script to a filepath
-    """
-    with open(filepath, "w") as f:
-        f.write(script)
+#SBATCH --job-name={name}
+#SBATCH --partition=compute
+#SBATCH --time=00:30:00
+#SBATCH --mem-per-cpu=1G
+#SBATCH --ntasks=1
+#SBATCH --mail-user={mail}
+#SBATCH --mail-type=BEGIN,FAIL,END
+#SBATCH --input=none
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
 
-def create_script(template_path, out_path="MyJob.sh", jobname="My_Job", mail="", script=""):
+{script}"""
+
+def create_script(out_path="MyJob.sh", jobname="Jobby McJobFace", mail="", script=""):
     """
     This function creates a sbatch job script file given a script to run.
-    Input: template_path - path to the template
-           out_path - path to the output. Default is MyJob.sh
-           jobname - name of the job. Default is "My_Job". Note: spaces will be removed
-           mail - email of the user. Default is blank.
-           script - script to run on the cluster.
+    Input: out_path - path to the output.
+           jobname - name of the job. Note: spaces will be removed
+           mail - email of the user.
+           script - bash script to run on the cluster.
     """
-    template = import_template(template_path)
 
     jobname = "".join(jobname.split()) # Quick way to remove all white spaces
 
-    template = template.replace("__JOBNAME__", jobname)
-    template = template.replace("__USERMAIL__", mail)
-    template = template.replace("__SCRIPT__", script)
+    job = template.format(name=jobname, mail=mail, script=script)
 
-    export_script(out_path, template)
+    with open(out_path, "w") as f:
+        f.write(job)
