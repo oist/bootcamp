02f92b7545a0d9a5e9b82827343eec26710d48b7
Jie <jie.gillet@gmail.com>
2019-03-04 17:51:56 +0900

    Removing blank code before cleanup

--- Blank code/BLAST/analysis.py
+++
@@ -1,5 +0,0 @@
-########## STEP 3 ##########
-# How many species had a hit?
-# How many hits per species?
-# What is the average alignment percentage per species?
-
--- Blank code/BLAST/main.py
+++
@@ -1,9 +0,0 @@
-########## STEP 1 ##########
-# We have two files with raw sequences we want to search, raw1.fa and raw2.fa
-# We are only interesetd in the fungi or Bacillus sequences
-# We need to read both files, extract and filer sequences and write them to a new file
-
-
-########## STEP 2 ##########
-# Prepare a SLURM file to run the query on Tombo
-
